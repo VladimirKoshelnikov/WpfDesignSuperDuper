6122ab84410b9282074d33cf7c2ab989d4d24793
Кошельников Владимир <vkoshelnikov@bk.ru>
2022-12-21 12:05:30 +0300

    Added ModelView on Front Panel

--- WpfDesignSuperDuper/MVVM/ViewModel/MainViewModel.cs
+++ WpfDesignSuperDuper/MVVM/ViewModel/MainViewModel.cs
@@ -9,8 +9,22 @@ namespace WpfDesignSuperDuper.MVVM.ViewModel
 {
     class MainViewModel : ObservableObject
     {
+        public HomeViewModel HomeVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
         public MainViewModel()
         {
+            HomeVM = new HomeViewModel();
+            CurrentView = HomeVM;
         }
     }
 }
